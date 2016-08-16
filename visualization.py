fd3319e1e12f4101097e449a3483af04e7bfd24f
Vit Mojzis <vmojzis@redhat.com>
2016-08-16 15:54:12 +0200

    Add licence:wq information to all source files.

--- visualization.py
+++ visualization.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# (C) Copyright 2016 Vit Mojzis, vmojzis@redhat.com
+# 
+# This program is distributed under the terms of the GNU General Public License
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import print_function
 #egg_path='/home/vmojzis/DEVEL/selinux-policy/sepolicy_analysis/networkx-1.10-py3.4.egg'
 
