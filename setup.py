cbfc1f14e79d2d7c79504fb39d20370cbe81300f
Vit Mojzis <vmojzis@redhat.com>
2017-02-08 12:29:21 +0100

    Fix setup.py to handle .conf files correctly.

--- setup.py
+++ setup.py
@@ -8,7 +8,11 @@ setup(
     version="0.1",
     packages=find_packages(exclude=["setools", "policy_data", "demo", "examples", "data"]),
     scripts=['extract_cil.sh', 'sebuild_graph', 'seexport_graph', 'segraph_query', 'sevisual_query'], #, 'export_graph.py', 'build_graph.py', 'visual_query.py', 'graph_query.py'
-    #config files
+    #keep config files
+    package_data={
+    # If any package contains *.conf files, include them:
+        '': ['*.conf']
+    },
     data_files=[
             ("/etc/sepolicyanalysis/", ['sepolicyanalysis/domain_groups_cil.conf', 'sepolicyanalysis/security_related.conf'])
         ],
@@ -17,7 +21,9 @@ setup(
     author="Vit Mojzis",
     author_email="vmojzis@redhat.com",
     description="Tool designed to help increase the quality of SELinux policy by identifying possibly dangerous permission pathways, simplifying regression testing and providing policy visualization.",
-    license="GPL-3.0",
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        ],
     keywords="SELinux policy analysis visualization security",
     url="https://github.com/vmojzis/sepolicy_analysis"   # github repository - wiki pages serve as documentation
 )
